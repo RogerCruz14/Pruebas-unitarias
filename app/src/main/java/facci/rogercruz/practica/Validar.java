c8b4125ae90e08eaa82310830293d3ba7d885c2c
MOCHASPLIT <guerrerozamora213@gmail.com>
2022-07-15 10:46:07 -0500

    Practica Unitarias

---
+++ app/src/main/java/facci/rogercruz/practica/Validar.java
@@ -0,0 +1,29 @@
+package facci.rogercruz.practica;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class Validar {
+
+    public boolean validarCampo(String cadena) {
+        if (cadena.length()== 0){
+            return false;
+        }
+        return true;
+    }
+
+    public boolean validarLargo(String cadena) {
+        if (cadena.length() >= 4){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean validarlargo(String cadena) {
+        if (cadena.length() >= 10){
+            return true;
+        }
+        return false;
+    }
+
+}
---
+++ app/src/test/java/facci/rogercruz/practica/ValidarTest.java
@@ -0,0 +1,26 @@
+package facci.rogercruz.practica;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+public class ValidarTest {
+
+    @Test
+    public void validarCampo_True() {
+        Validar validar = new Validar();
+        assertTrue(validar.validarCampo("Nombre"));
+    }
+    @Test
+    public void validarLargo_True() {
+        Validar validar = new Validar();
+        assertTrue(validar.validarLargo("Nomb"));
+    }
+    @Test
+    public void validarlargo_True() {
+        Validar validar = new Validar();
+        assertTrue(validar.validarlargo("Nombre3rferf"));
+    }
+
+}
